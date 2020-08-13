2dd73cb425a9cd03de133ce2280b11cad7a45987
nina273 <lukanovic.nikolina@gmail.com>
2020-08-14 01:33:16 +0200

    register-message_alert

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -3,6 +3,7 @@ import { ValidateService } from '../../services/validate.service';
 import { FlashMessagesService } from 'angular2-flash-messages';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-register',
@@ -21,10 +22,16 @@ export class RegisterComponent implements OnInit {
     private validateService: ValidateService,
     private flashMessage: FlashMessagesService,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {}
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 2500,
+  })
+}
   onRegisterSubmit() {
     const user = {
       name: this.name,
@@ -37,34 +44,38 @@ export class RegisterComponent implements OnInit {
 
     //required fields
     if (!this.validateService.validateRegister(user)) {
-      this.flashMessage.show('Please fill all fields', {
+      this.openSnackBar("Please fill all fields","Warning")
+      /* this.flashMessage.show('Please fill all fields', {
         cssClass: 'alert-danger',
         timeout: 3000,
-      });
+      });*/
       return false;
     }
 
     //validate email
     if (!this.validateService.validateEmail(user.email)) {
-      this.flashMessage.show('Please use a valid email', {
+      this.openSnackBar("Please use a valid email","Warning")
+      /*this.flashMessage.show('Please use a valid email', {
         cssClass: 'alert-danger',
         timeout: 3000,
-      });
+      });*/
       return false;
     }
     //register user
     this.authService.registerUser(user).subscribe((data) => {
       if (data.success) {
-        this.flashMessage.show('You are now registered and can log in', {
+        this.openSnackBar("You are now registered and can log in","Success")
+        /*this.flashMessage.show('You are now registered and can log in', {
           cssClass: 'alert-success',
           timeout: 3000,
-        });
+        });*/
         this.router.navigate(['/login']); //redirakcija nakon uspješne registracije
       } else {
-        this.flashMessage.show('Something went wrong!', {
+        this.openSnackBar("Something went wrong","Warning")
+        /*this.flashMessage.show('Something went wrong!', {
           cssClass: 'alert-danger',
           timeout: 3000,
-        });
+        });*/
         this.router.navigate(['/register']);
       }
     });
